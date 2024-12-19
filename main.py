8477f236b9a50c79559853c1c0261e81ba353b2d
Dmytro <dimasirko@gmail.com>
2024-12-19 11:29:11 +0200

    finish

--- main.py
+++ main.py
@@ -6,3 +6,31 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER = '[name]'
+
+def read_example(lines):
+    with open("Input/Letters/starting_letter.txt", "r") as starting_letter:
+        ex_letter = starting_letter.read()
+
+        for line in lines:
+            filename = f'Output/ReadyToSend/letter_to_{line}.txt'
+            strip_line = line.strip()
+            new_ex_letter = ex_letter.replace(PLACEHOLDER, strip_line)
+            print(new_ex_letter)
+            
+            with open(filename, 'w') as f:
+                f.write(new_ex_letter)
+
+    return "Done"
+        
+
+def read_names():
+    with open('Input/Names/invited_names.txt', 'r') as file:
+        lines = file.readlines()
+        return lines
+
+
+if __name__ == "__main__":
+    print(read_example(read_names()))
+
